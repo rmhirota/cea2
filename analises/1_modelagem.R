85681f414d380039c40abb73c883485983d21193
giovendeiro <giovendeiro@usp.br>
2021-11-02 14:33:36 -0300

    friedman test grupo 1

--- analises/1_modelagem.R
+++ analises/1_modelagem.R
@@ -119,3 +119,115 @@ ks.test(baby_12$diff_video,"pexp", fit2$estimate) # p-value < 0.05 -> distributi
 #  ties should not be present for the Kolmogorov-Smirnov test
 
 
+
+##Teste de Friedman entre dias do mesmo bebê
+# Grupo 1
+
+baby_11 <- as.data.frame(da_diff_tempo %>%
+                           dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[1], dia == 1) %>%
+                           dplyr::select(diff_video))
+
+
+baby_12 <- da_diff_tempo %>%
+  dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[1], dia == 2) %>%
+  dplyr::select(diff_video)
+
+baby_13 <- da_diff_tempo %>%
+  dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[1], dia == 3) %>%
+  dplyr::select(diff_video)
+
+df1 = matrix(c(baby_11$diff_video[1:18],baby_12$diff_video[1:18], baby_13$diff_video[1:18]), 18, 3)
+
+friedman.test(df1)
+#Friedman rank sum test
+
+#data:  df1
+#Friedman chi-squared = 1.4444, df = 2, p-value = 0.4857
+
+
+baby_21 <- as.data.frame(da_diff_tempo %>%
+                           dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[2], dia == 1) %>%
+                           dplyr::select(diff_video))
+
+
+baby_22 <- da_diff_tempo %>%
+  dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[2], dia == 2) %>%
+  dplyr::select(diff_video)
+
+baby_23 <- da_diff_tempo %>%
+  dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[2], dia == 3) %>%
+  dplyr::select(diff_video)
+
+df2 = matrix(c(baby_21$diff_video[1:18],baby_22$diff_video[1:26], baby_23$diff_video[1:26]), 26, 3)
+
+friedman.test(df2)
+#Friedman rank sum test
+
+#data:  df2
+#Friedman chi-squared = 13, df = 2, p-value = 0.001503
+
+baby_31 <- as.data.frame(da_diff_tempo %>%
+                           dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[3], dia == 1) %>%
+                           dplyr::select(diff_video))
+
+
+baby_32 <- da_diff_tempo %>%
+  dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[3], dia == 2) %>%
+  dplyr::select(diff_video)
+
+baby_33 <- da_diff_tempo %>%
+  dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[3], dia == 3) %>%
+  dplyr::select(diff_video)
+
+df3 = matrix(c(baby_31$diff_video[1:18],baby_32$diff_video[1:18], baby_33$diff_video[1:18]), 18, 3)
+
+friedman.test(df3)
+#Friedman chi-squared = NaN, df = 2, p-value = NA
+
+
+baby_41 <- as.data.frame(da_diff_tempo %>%
+                           dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[4], dia == 1) %>%
+                           dplyr::select(diff_video))
+
+
+baby_42 <- da_diff_tempo %>%
+  dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[4], dia == 2) %>%
+  dplyr::select(diff_video)
+
+baby_43 <- da_diff_tempo %>%
+  dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[4], dia == 3) %>%
+  dplyr::select(diff_video)
+
+df4 = matrix(c(baby_41$diff_video[1:63],baby_42$diff_video[1:63], baby_43$diff_video[1:63]), 63, 3)
+
+friedman.test(df4)
+#Friedman rank sum test
+
+#data:  df4
+#Friedman chi-squared = 17.568, df = 2, p-value = 0.0001532
+
+
+
+baby_51 <- as.data.frame(da_diff_tempo %>%
+                           dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[5], dia == 1) %>%
+                           dplyr::select(diff_video))
+
+
+baby_52 <- da_diff_tempo %>%
+  dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[5], dia == 2) %>%
+  dplyr::select(diff_video)
+
+baby_53 <- da_diff_tempo %>%
+  dplyr::filter(!is.na(diff_video),condicao == 'contingente',nome==nomes_b1[5], dia == 3) %>%
+  dplyr::select(diff_video)
+
+df5 = matrix(c(baby_51$diff_video[1:5],baby_52$diff_video[1:5], baby_53$diff_video[1:5]), 5, 3)
+
+friedman.test(df5)
+
+#Friedman rank sum test
+
+#data:  df5
+#Friedman chi-squared = 2.8, df = 2, p-value = 0.2466
+
+
