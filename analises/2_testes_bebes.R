0ed3c6423f8a09319b9639fe1f361159900a5982
rmhirota <re.hirota@gmail.com>
2021-11-18 21:26:09 -0300

    testes kruskal wallis

--- analises/2_testes_bebes.R
+++ analises/2_testes_bebes.R
@@ -57,29 +57,80 @@ grafico <- function(g, n) {
 
 purrr::map2(bbs$grupo, bbs$nome, grafico)
 
-# Wilcoxon: Comparação entre dias
+
+# Kruskal-Wallis ----------------------------------------------------------
+
 da_spss <- readr::read_rds("data-raw/2_tidy_spss.rds")
 
-dia1 = as.matrix(da_spss %>% dplyr::filter(dia==1) %>% dplyr::select(freq_apertos))
-dia2 = as.matrix(da_spss %>% dplyr::filter(dia==2) %>% dplyr::select(freq_apertos))
-dia3 = as.matrix(da_spss %>% dplyr::filter(dia==3) %>% dplyr::select(freq_apertos))
-
-teste_w <- function(d, n) {
-  wilcox.test(d,n)
+teste_kw <- function(cond, d) {
+  da <- da_spss %>%
+    dplyr::filter(condicao == cond, dia == d) %>%
+    dplyr::select(grupo, freq_apertos)
+  g1 <- dplyr::filter(da, grupo == 1)
+  g2 <- dplyr::filter(da, grupo == 2)
+  g3 <- dplyr::filter(da, grupo == 3)
+  kruskal.test(list(
+    g1$freq_apertos, g2$freq_apertos, g3$freq_apertos
+  ))
 }
 
-teste_w(dia1,dia2) #p-value = 0.2476, não rejeita a igualdade
-teste_w(dia1,dia3) #p-value = 0.3803, não rejeita a igualdade
-teste_w(dia2,dia3) #p-value = 0.6977, não rejeita a igualdade
+# Contingente
+teste_kw("c", 1)  # dia 1
+teste_kw("c", 2)  # dia 2
+teste_kw("c", 3)  # dia 3
 
-# Wilcoxon: Comparação entre grupos
+# Basal 1
+teste_kw("bas", 1)  # dia 1
+teste_kw("bas", 2)  # dia 2
+teste_kw("bas", 3)  # dia 3
 
-grupo1 = as.matrix(da_spss %>% dplyr::filter(grupo==1) %>% dplyr::select(freq_apertos))
-grupo2 = as.matrix(da_spss %>% dplyr::filter(grupo==2) %>% dplyr::select(freq_apertos))
-grupo3 = as.matrix(da_spss %>% dplyr::filter(grupo==3) %>% dplyr::select(freq_apertos))
+# Basal 2
+teste_kw("pos", 1)  # dia 1
+teste_kw("pos", 2)  # dia 2
+teste_kw("pos", 3)  # dia 3
+
+# Não contingente
+teste_kw("nc", 1)  # dia 1
+teste_kw("nc", 2)  # dia 2
+teste_kw("nc", 3)  # dia 3
 
 
-teste_w(grupo1,grupo2) #p-value = p-value = 0.004112, nrejeita-se a igualdade
-teste_w(grupo1,grupo3) #p-value = 0.008848, não rejeita a igualdade
-teste_w(grupo2,grupo3) # p-value = 0.8918, não rejeita a igualdade
+# Wilcoxon ----------------------------------------------------------------
+
+# Wilcoxon: Comparação entre dias (basal 2 - basal 1)
+da_spss <- readr::read_rds("data-raw/2_tidy_spss.rds")
+
+da_wilcoxon <- da_spss %>%
+  dplyr::filter(condicao %in% c("bas", "pos")) %>%
+  dplyr::mutate(id = paste0(nome, grupo)) %>%
+  dplyr::select(id, dia, condicao, freq_apertos) %>%
+  tidyr::pivot_wider(names_from = condicao, values_from = freq_apertos) %>%
+  dplyr::mutate(dif_basal = (pos - bas)/bas)
+
+teste_w <- function(dia1, dia2, alt = "two.sided") {
+  d1 <- da_wilcoxon %>%
+    dplyr::filter(dia == dia1) %>%
+    dplyr::pull(dif_basal)
+  d2 <- da_wilcoxon %>%
+    dplyr::filter(dia == dia2) %>%
+    dplyr::pull(dif_basal)
+  wilcox.test(d1, d2, paired = TRUE, alternative = alt)
+}
+
+# teste bicaudal
+teste_w(1, 2)  # diferença entre dias 1 e 2
+teste_w(1, 3)  # diferença entre dias 1 e 3
+teste_w(2, 3)  # diferença entre dias 2 e 3
+
+# teste H_1: Md1 > Md2
+teste_w(1, 2, "greater")  # diferença entre dias 1 e 2
+teste_w(1, 3, "greater")  # diferença entre dias 1 e 3
+teste_w(2, 3, "greater")  # diferença entre dias 2 e 3
+
+# teste H_1: Md1 < Md2
+teste_w(1, 2, "less")  # diferença entre dias 1 e 2
+teste_w(1, 3, "less")  # diferença entre dias 1 e 3
+teste_w(2, 3, "less")  # diferença entre dias 2 e 3
+
+
 
