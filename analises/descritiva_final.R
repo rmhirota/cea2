b508a4acd5b79d6a3a0e37ac906b24b2a88f8df7
m-almeida <mariana_almeidacosta@usp.br>
2022-01-02 13:35:27 -0300

    organização do código

--- analises/descritiva_final.R
+++ analises/descritiva_final.R
@@ -1,5 +1,15 @@
+# Pacotes ----------------------------------------------------------------------------
+
 library(magrittr)
 library(patchwork)
+library(htmlwidgets)
+library(webshot)
+library(readr)
+
+# Leitura das bases -------------------------------------------------------------------
+
+da_tidy <- read.csv("DADOS/BASES INTERMEDIÁRIAS/da_tidy") # inserir o caminho do arquivo csv da_tidy (base bruta)
+da_spss <- readr::read_rds("DADOS/BASES INTERMEDIÁRIAS/2_tidy_spss.rds") # inserir o caminho do arquivo rds (base spss)
 
 # Análise descritiva
 
@@ -77,6 +87,7 @@ cea2::da_tidy %>%
   ggplot2::scale_fill_manual(values = c("#440154FF", "#21908CFF", "#FDE725FF"))
 
 # Pressão Média ao longo do tempo ------------------------------------------------------
+## qual o código da função hc_grupo?
 
 g1 = cea2::hc_grupo("b1") %>%
   highcharter::hc_title(text = "Pressão média por segundo - Grupo 1")
@@ -93,7 +104,7 @@ webshot::webshot(url="analises/bas_c_interativo_g2.html",file="analises/grafico2
 g3 = cea2::hc_grupo("b3") %>%
   highcharter::hc_title(text = "Pressão média por segundo - Grupo 3")
 
-htmlwidgets::saveWidget(widget=g3,file="analises/bas_c_interativo_g3.html")
+htmlwidgets::saveWidget(widget=g3,file="analises/bas_c_interativo_g3.html") # salva os graficos no caminho passado
 webshot::webshot(url="analises/bas_c_interativo_g3.html",file="analises/grafico3.png",delay=20)
 
 
@@ -146,7 +157,7 @@ ggplot2::ggsave("analises/perfis_grupo3.png", grupo3,width = 7, height = 7)
 ggplot2::ggsave("pres/plots/perfis_grupo3.png", grupo3,width = 7, height = 7)
 
 
-# Frequencia de apertos boxplot
+# Frequencia de apertos boxplot -------------------------------------------------------
 
 da_spss <- readr::read_rds("../data-raw/2_tidy_spss.rds")
 boxplot_freq <- function(cond) {
@@ -243,7 +254,6 @@ da_dia <- da_spss %>%
     condicao == "Basal 1" ~ "Basal_1",
     TRUE ~ "Basal_2"
   )) %>%
-  #dplyr::filter(grupo == 1) %>%
   tidyr::pivot_wider(names_from = condicao, values_from = freq_apertos)
 
 #Dia 1
@@ -327,3 +337,260 @@ ggplot2::ggsave("pres/plots/tempo_video_g2.jpeg", tempo_grupo2)
 tempo_grupo3 <- tempo_video(da_diff_tempo, "b3","contingente")
 ggplot2::ggsave("analises/tempo_video_g3.jpeg", tempo_grupo3)
 ggplot2::ggsave("pres/plots/tempo_video_g3.jpeg", tempo_grupo3)
+
+# Análise Inferencial
+
+# Kruskal-Wallis ----------------------------------------------------------
+
+da_spss <- readr::read_rds("data-raw/2_tidy_spss.rds")
+
+teste_kw <- function(cond, d) {
+  da <- da_spss %>%
+    dplyr::filter(condicao == cond, dia == d) %>%
+    dplyr::select(grupo, freq_apertos)
+  g1 <- dplyr::filter(da, grupo == 1)
+  g2 <- dplyr::filter(da, grupo == 2)
+  g3 <- dplyr::filter(da, grupo == 3)
+  kruskal.test(list(
+    g1$freq_apertos, g2$freq_apertos, g3$freq_apertos
+  ))
+}
+
+# Contingente
+d1 <- teste_kw("c", 1)
+d2 <- teste_kw("c", 2)
+d3 <- teste_kw("c", 3)
+
+dplyr::bind_cols(
+  Dia = c("Dia 1", "Dia 2","Dia 3"),
+  purrr::map_df(list(d1,d2,d3), broom::tidy)
+) %>%
+  dplyr::select(-parameter, -method) %>%
+  knitr::kable()
+
+# Não Contingente
+
+n1 <- teste_kw("nc", 1)
+n2 <- teste_kw("nc", 2)
+n3 <- teste_kw("nc", 3)
+
+dplyr::bind_cols(
+  Dia = c("Dia 1", "Dia 2", "Dia 3"),
+  purrr::map_df(list(n1, n2, n3), broom::tidy)
+) %>%
+  dplyr::select(-parameter, -method) %>%
+  knitr::kable()
+
+# Basal 1
+b1 <- teste_kw("bas", 1)
+b2 <- teste_kw("bas", 2)
+b3 <- teste_kw("bas", 3)
+
+dplyr::bind_cols(
+  Dia = c("Dia 1", "Dia 2", "Dia 3"),
+  purrr::map_df(list(b1, b2, b3), broom::tidy)
+) %>%
+  dplyr::select(-parameter, -method) %>%
+  knitr::kable()
+
+# Basal 2
+p1 <- teste_kw("pos", 1)
+p2 <- teste_kw("pos", 2)
+p3 <- teste_kw("pos", 3)
+
+dplyr::bind_cols(
+  Dia = c("Dia 1", "Dia 2", "Dia 3"),
+  purrr::map_df(list(p1, p2, p3), broom::tidy)
+) %>%
+  dplyr::select(-parameter, -method) %>%
+  knitr::kable()
+
+# Wilcoxon --------------------------------------------------------------------------
+
+# Wilcoxon: Comparação entre dias (basal 2 - basal 1)
+da_spss <- readr::read_rds("data-raw/2_tidy_spss.rds")
+
+da_wilcoxon <- da_spss %>%
+  dplyr::filter(condicao %in% c("pos")) %>%
+  dplyr::mutate(id = paste0(nome, grupo)) %>%
+  dplyr::select(id, dia, condicao, freq_apertos) %>%
+  tidyr::pivot_wider(names_from = condicao, values_from = freq_apertos)
+
+teste_w2 <- function(dia1, dia2, alt = "greater") {
+  d1 <- da_wilcoxon %>%
+    dplyr::filter(dia == dia1) %>%
+    dplyr::pull(pos)
+  d2 <- da_wilcoxon %>%
+    dplyr::filter(dia == dia2) %>%
+    dplyr::pull(pos)
+  wilcox.test(d1, d2, paired = TRUE, alternative = alt)
+}
+
+w1 <- teste_w2(1, 2)  # diferença entre dias 1 e 2
+w2 <- teste_w2(1, 3)  # diferença entre dias 1 e 3
+w3 <- teste_w2(2, 3)  # diferença entre dias 2 e 3
+
+dplyr::bind_cols(
+  Dia = c("Dia 1 x Dia 2", "Dia 1 x Dia 3", "Dia 2 x Dia 3"),
+  purrr::map_df(list(w1, w2, w3), broom::tidy)
+) %>%
+  knitr::kable()
+
+# Teste da exponencialização da variavel tempo até aperto ---------------------------
+
+da_diff_aperto <- cea2::da_tidy %>%
+  dplyr::group_by(grupo, nome, dia, condicao) %>%
+  dplyr::arrange(grupo, nome, dia, condicao, tempo) %>%
+  dplyr::filter(status_aperto %in% c("inicio", "fim")) %>%
+  dplyr::mutate(diff_aperto = dplyr::case_when(
+    status_aperto == "inicio" & dplyr::lag(status_aperto) == "fim"
+    ~ (tempo - dplyr::lag(tempo))
+  )) %>%
+  dplyr::ungroup()
+
+nomes_b1 <- cea2::da_tidy %>%
+  dplyr::filter(grupo == "b1") %>%
+  dplyr::pull(nome) %>% unique()
+nomes_b2 <- cea2::da_tidy %>%
+  dplyr::filter(grupo == "b2") %>%
+  dplyr::pull(nome) %>% unique()
+nomes_b3 <- cea2::da_tidy %>%
+  dplyr::filter(grupo == "b3") %>%
+  dplyr::pull(nome) %>% unique()
+
+
+graf_exp <- function(da, d, gr,c){
+  da_bebe <- da %>%
+    dplyr::filter(
+      !is.null(diff_aperto), dia == d,
+      grupo == gr,  condicao == c
+    )
+  da_bebe %>%
+    ggplot2::ggplot(ggplot2::aes(x = diff_aperto, y=..density..)) +
+    ggplot2::geom_histogram()
+}
+
+# Fixamos o dia 1 para análise
+# Grupo 1
+
+graf_exp(da_diff_aperto, 1, "b1","basal1") +
+  graf_exp(da_diff_aperto, 1, "b1","basal2") +
+  graf_exp(da_diff_aperto, 1, "b1","contingente") +
+  graf_exp(da_diff_aperto, 1, "b1","não contingente")
+
+graf_exp(da_diff_aperto, 2, "b1","basal1") +
+  graf_exp(da_diff_aperto, 2, "b1","basal2") +
+  graf_exp(da_diff_aperto, 2, "b1","contingente") +
+  graf_exp(da_diff_aperto, 2, "b1","não contingente")
+
+graf_exp(da_diff_aperto, 3, "b1","basal1") +
+  graf_exp(da_diff_aperto, 3, "b1","basal2") +
+  graf_exp(da_diff_aperto, 3, "b1","contingente") +
+  graf_exp(da_diff_aperto, 3, "b1","não contingente")
+
+# Grupo 2
+
+graf_exp(da_diff_aperto, 1, "b2","basal1") +
+  graf_exp(da_diff_aperto, 1, "b2","basal2") +
+  graf_exp(da_diff_aperto, 1, "b2","contingente") +
+  graf_exp(da_diff_aperto, 1, "b2","não contingente")
+
+graf_exp(da_diff_aperto, 2, "b2","basal1") +
+  graf_exp(da_diff_aperto, 2, "b2","basal2") +
+  graf_exp(da_diff_aperto, 2, "b2","contingente") +
+  graf_exp(da_diff_aperto, 2, "b2","não contingente")
+
+graf_exp(da_diff_aperto, 3, "b2","basal1") +
+  graf_exp(da_diff_aperto, 3, "b2","basal2") +
+  graf_exp(da_diff_aperto, 3, "b2","contingente") +
+  graf_exp(da_diff_aperto, 3, "b2","não contingente")
+
+# Grupo 3
+
+graf_exp(da_diff_aperto, 1, "b3","basal1") +
+  graf_exp(da_diff_aperto, 1, "b3","basal2") +
+  graf_exp(da_diff_aperto, 1, "b3","contingente") +
+  graf_exp(da_diff_aperto, 1, "b3","não contingente")
+
+graf_exp(da_diff_aperto, 2, "b3","basal1") +
+  graf_exp(da_diff_aperto, 2, "b3","basal2") +
+  graf_exp(da_diff_aperto, 2, "b3","contingente") +
+  graf_exp(da_diff_aperto, 2, "b3","não contingente")
+
+graf_exp(da_diff_aperto, 3, "b3","basal1") +
+  graf_exp(da_diff_aperto, 3, "b3","basal2") +
+  graf_exp(da_diff_aperto, 3, "b3","contingente") +
+  graf_exp(da_diff_aperto, 3, "b3","não contingente")
+
+# Teste de KS da variavel tempo entre apertos
+
+require(vcd)
+require(MASS)
+
+teste <- function(c,d,g){
+  base = as.data.frame(da_diff_aperto %>%
+                         dplyr::filter(!is.na(diff_aperto),condicao == c,dia== d, grupo == g) %>%
+                         dplyr::select(diff_aperto))
+  fit = fitdistr(base$diff_aperto,"exponential")
+  ks.test(base$diff_aperto,"pexp", fit$estimate)
+}
+
+# Grupo 1
+
+#todos os testes foram rejeitados
+
+teste('basal1',1,'b1')
+teste('basal2',1,'b1')
+teste('contingente',1,'b1')
+teste('não contingente',1,'b1')
+
+teste('basal1',2,'b1')
+teste('basal2',2,'b1')
+teste('contingente',2,'b1')
+teste('não contingente',2,'b1')
+
+teste('basal1',3,'b1')
+teste('basal2',3,'b1')
+teste('contingente',3,'b1')
+teste('não contingente',3,'b1')
+
+# Grupo 2
+
+#todos os testes foram rejeitados
+
+teste('basal1',1,'b2')
+teste('basal2',1,'b2')
+teste('contingente',1,'b2')
+teste('não contingente',1,'b2')
+
+teste('basal1',2,'b2')
+teste('basal2',2,'b2')
+teste('contingente',2,'b2')
+teste('não contingente',2,'b2')
+
+teste('basal1',3,'b2')
+teste('basal2',3,'b2')
+teste('contingente',3,'b2')
+teste('não contingente',3,'b2')
+
+# Grupo 3
+
+#todos os testes foram rejeitados
+
+teste('basal1',1,'b3')
+teste('basal2',1,'b3')
+teste('contingente',1,'b3')
+teste('não contingente',1,'b3')
+
+teste('basal1',2,'b3')
+teste('basal2',2,'b3')
+teste('contingente',2,'b3')
+teste('não contingente',2,'b3')
+
+teste('basal1',3,'b3')
+teste('basal2',3,'b3')
+teste('contingente',3,'b3')
+teste('não contingente',3,'b3')
+
+
+
